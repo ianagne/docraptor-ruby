2c0b2747e3e88a6e9f040807fc6745fc78554065
Ian Agne <ian.d.agne@gmail.com>
2021-05-11 21:34:34 -0500

    Support Ruby 3.0

--- test/hosted_sync.rb
+++ test/hosted_sync.rb
@@ -1,28 +1,28 @@
-require "bundler/setup"
+require 'bundler/setup'
 Bundler.require
-require "open-uri"
+require 'open-uri'
 
 DocRaptor.configure do |dr|
-  dr.username  = "YOUR_API_KEY_HERE"
+  dr.username = 'YOUR_API_KEY_HERE'
   # dr.debugging = true
 end
 
 $docraptor = DocRaptor::DocApi.new
 
-output_file = "hosted-ruby-sync.pdf"
+output_file = 'hosted-ruby-sync.pdf'
 
 output_payload = $docraptor.create_hosted_doc(
-  test:             true,
-  document_content: "<html><body>Hello from Ruby</body></html>",
-  name:             output_file,
-  document_type:    "pdf",
-  hosted_expires_at: (Time.now + 86400).strftime('%FT%T%:z'), # 1 day from now
+  test: true,
+  document_content: '<html><body>Hello from Ruby</body></html>',
+  name: output_file,
+  document_type: 'pdf',
+  hosted_expires_at: (Time.now + 86_400).strftime('%FT%T%:z') # 1 day from now
 )
 
-actual_document = open output_payload.download_url
+actual_document = URI.open output_payload.download_url
 IO.copy_stream(actual_document, output_file)
 
 output_type = `file -b #{output_file}`
 File.delete output_file
 
-raise "Output was not a PDF" unless output_type.start_with?("PDF")
+raise 'Output was not a PDF' unless output_type.start_with?('PDF')
--- test/xlsx.rb
+++ test/xlsx.rb
@@ -1,24 +1,24 @@
-require "bundler/setup"
+require 'bundler/setup'
 Bundler.require
 
 DocRaptor.configure do |dr|
-  dr.username  = "YOUR_API_KEY_HERE"
+  dr.username = 'YOUR_API_KEY_HERE'
   # dr.debugging = true
 end
 
 $docraptor = DocRaptor::DocApi.new
 
-output_file = "ruby-xlsx.xlsx"
+output_file = 'ruby-xlsx.xlsx'
 
 output_payload = $docraptor.create_doc(
-  test:             true,
-  document_content: "<html><body><table><tr><td>Hello from Ruby</td></tr></table></body></html>",
-  name:             output_file,
-  document_type:    "xlsx",
+  test: true,
+  document_content: '<html><body><table><tr><td>Hello from Ruby</td></tr></table></body></html>',
+  name: output_file,
+  document_type: 'xlsx'
 )
 
 File.write(output_file, output_payload)
 output_type = `file -b #{output_file}`
 File.delete output_file
 
-raise "Output was not an XLSX (zip)" unless output_type.start_with?("Zip")
+raise 'Output was not an XLSX (zip)' unless output_type.start_with?('Zip')
